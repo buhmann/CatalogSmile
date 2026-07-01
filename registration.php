1d2a685a8872572706ac540dd4c1cf548721d4e4
buhmann <buhmann81@gmail.com>
2026-07-01 11:14:45 +0200

    Initial commit

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Copyright © Buhmann. All rights reserved.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Buhmann_CatalogSmile', __DIR__);
